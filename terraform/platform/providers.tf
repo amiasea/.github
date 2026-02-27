16c3c8adda76e2ea78aa903f662a9dda75b0215e
Alfredo Ball <alfredo.ball@outlook.com>
2026-02-27 14:29:47 -0600

    Initial

--- terraform/platform/providers.tf
+++ terraform/platform/providers.tf
@@ -2,11 +2,19 @@ terraform {
   required_version = ">= 1.6.0"
 
   required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.60.0" # Use a modern version for Project support
+    }
     hcp = {
       source  = "hashicorp/hcp"
       version = ">= 0.104.0"
     }
   }
+}
+
+provider "tfe" {
+  # Authenticates via the TF_TOKEN_app_terraform_io env var you already have
 }
 
 provider "hcp" {
