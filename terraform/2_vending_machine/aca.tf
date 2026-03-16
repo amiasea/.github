8db553b42e7b16577db0fa44f4ef20389af7eee5
Alfredo Ball <alfredo.ball@outlook.com>
2026-03-16 18:37:46 -0500

    initial

--- terraform/2_vending_machine/aca.tf
+++ terraform/2_vending_machine/aca.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_app_environment" "main" {
   provider = azurerm.sub
-  name                = "amiasea-env"
+  name                = "cae-${var.prefix}-${var.env}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
 
