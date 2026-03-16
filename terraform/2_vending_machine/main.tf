eacadfc11fc9ab4a180a0b2b233d9adc002b1574
Alfredo Ball <alfredo.ball@outlook.com>
2026-03-16 11:31:48 -0500

    initial

--- terraform/2_vending_machine/main.tf
+++ terraform/2_vending_machine/main.tf
@@ -25,3 +25,9 @@ resource "azurerm_user_assigned_identity" "uami" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
 }
+
+resource "azurerm_role_assignment" "terraform_kv_admin" {
+  scope                = var.sovereign_key_vault_id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.uami.principal_id
+}
