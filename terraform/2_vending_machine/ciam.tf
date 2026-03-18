05abb6cbca1064c385992adfc8355944132cf3cf
Alfredo Ball <alfredo.ball@outlook.com>
2026-03-18 14:30:51 -0500

    initial

--- terraform/2_vending_machine/ciam.tf
+++ terraform/2_vending_machine/ciam.tf
@@ -2,6 +2,8 @@ data "azapi_resource" "ciam_tenant" {
   type      = "Microsoft.AzureActiveDirectory/ciamDirectories@2025-08-01-preview"
   name      = "aviatortenant${var.env}.onmicrosoft.com"
   parent_id = data.azurerm_resource_group.rg.id
+
+  response_export_values = ["*"]
 }
 
 resource "azuread_user_flow_attribute" "organization" {
@@ -9,4 +11,9 @@ resource "azuread_user_flow_attribute" "organization" {
   display_name = "Organization"
   data_type    = "string"
   description  = "Company or Org name"
+}
+
+output "ciam_tenant_id" {
+  # Now that we're catching the values, we can parse the string
+  value = jsondecode(data.azapi_resource.ciam_tenant.output).properties.tenantId
 }
