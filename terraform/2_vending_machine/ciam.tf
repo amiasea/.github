9c1ed1d800fd32aa47c200f7361c75fd924120e5
Alfredo Ball <alfredo.ball@outlook.com>
2026-03-19 14:22:06 -0500

    initial

--- terraform/2_vending_machine/ciam.tf
+++ terraform/2_vending_machine/ciam.tf
@@ -13,58 +13,104 @@
 #   description  = "Company or Org name"
 # }
 
-resource "azapi_resource" "external_tenant" {
-#   for_each  = toset(var.environments)
-  type      = "Microsoft.AzureActiveDirectory/ciamDirectories@2023-05-17-preview"
-  name      = "aviatortenanttest.onmicrosoft.com"
-  parent_id = data.azurerm_resource_group.rg.id
-  ignore_casing = true
+# resource "azapi_resource" "external_tenant" {
+# #   for_each  = toset(var.environments)
+#   type      = "Microsoft.AzureActiveDirectory/ciamDirectories@2023-05-17-preview"
+#   name      = "aviatortenanttest.onmicrosoft.com"
+#   parent_id = data.azurerm_resource_group.rg.id
+#   ignore_casing = true
 
-  schema_validation_enabled = false
-  location                  = "United States"
+#   schema_validation_enabled = false
+#   location                  = "United States"
 
-    body = merge(
-    {
-      properties = merge(
-        {
-          createTenantProperties = {
-            countryCode = "US"
-            displayName = "Aviator Test"
-          }
+#     body = merge(
+#     {
+#       properties = merge(
+#         {
+#           createTenantProperties = {
+#             countryCode = "US"
+#             displayName = "Aviator Test"
+#           }
 
-          billingConfig = {
-            billingType = "MAU"
-          }
-        },
-        {
-            initialDomainAdministrator = {
-                userPrincipalName = "alfredo.ball@amiasea.onmicrosoft.com"
-                displayName       = "Alfredo Ball"
-                # passwordProfile = {
-                #     password                      = var.initial_domain_administrator.password
-                #     forceChangePasswordNextSignIn = true
-                # }
-                # accountEnabled = true
-            }
-        }
-      )
+#           billingConfig = {
+#             billingType = "MAU"
+#           }
+#         },
+#         {
+#             initialDomainAdministrator = {
+#                 userPrincipalName = "alfredo.ball@amiasea.onmicrosoft.com"
+#                 displayName       = "Alfredo Ball"
+#                 # passwordProfile = {
+#                 #     password                      = var.initial_domain_administrator.password
+#                 #     forceChangePasswordNextSignIn = true
+#                 # }
+#                 # accountEnabled = true
+#             }
+#         }
+#       )
 
-      sku = {
-        name = "Base"
-        tier = "A0"
+#       sku = {
+#         name = "Base"
+#         tier = "A0"
+#       }
+#     }
+#   )
+
+#   timeouts {
+#     create = "30m"
+#     update = "30m"
+#     delete = "30m"
+#   }
+
+#   response_export_values = [
+#     "properties.tenantId",
+#     "properties.domainName",
+#     "properties.provisioningState"
+#   ]
+# }
+
+resource "azapi_resource_action" "external_tenant_post" {
+  type        = "Microsoft.AzureActiveDirectory/ciamDirectories@2023-05-17-preview"
+  resource_id = "/subscriptions/bd0f2cca-0676-49e6-a8c2-cae21ea7216b/resourceGroups/rg-amiasea-dev/providers/Microsoft.AzureActiveDirectory/ciamDirectories/aviatortenanttest.onmicrosoft.com"
+  method      = "POST"
+
+  body = jsonencode({
+    location = "United States"
+    sku = {
+      name = "Base"
+      tier = "A0"
+    }
+    properties = {
+      createTenantProperties = {
+        displayName = "Aviator Test"
+        countryCode = "US"
+      }
+      billingConfig = {
+        billingType = "MAU"
+      }
+      initialDomainAdministrator = {
+        userPrincipalName = "alfredo.ball@amiasea.onmicrosoft.com"
+        displayName       = "Alfredo Ball"
+        # accountEnabled  = true
+        # passwordProfile = {
+        #   password                      = "YourPassword123!"
+        #   forceChangePasswordNextSignIn = true
+        # }
       }
     }
-  )
+  })
 
-  timeouts {
-    create = "30m"
-    update = "30m"
-    delete = "30m"
-  }
-
+  # Export values to capture the resulting tenant details
   response_export_values = [
     "properties.tenantId",
     "properties.domainName",
     "properties.provisioningState"
   ]
+
+  # Matches the specific timeouts from your original resource
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
 }
