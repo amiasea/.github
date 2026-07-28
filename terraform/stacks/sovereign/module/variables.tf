084d3689313bbfb0527824314509d5bae06da20d
Alfredo Ball <alfredo.ball@outlook.com>
2026-07-28 03:58:21 -0500

    sovereign stack - 10

--- terraform/stacks/sovereign/module/variables.tf
+++ terraform/stacks/sovereign/module/variables.tf
@@ -15,12 +15,10 @@ variable "tfe_org_token" {
 
 variable "azure_tenant_id" {
   type = string
-  ephemeral = true
 }
 
 variable "azure_subscription_id" {
   type = string
-  ephemeral = true
 }
 
 variable "key_vault_name" {
