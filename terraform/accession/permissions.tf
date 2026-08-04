474f99c895d5648226be6f7da4ab7a764582e33e
Alfredo Ball <alfredo.ball@outlook.com>
2026-08-04 09:42:07 -0500

    azurerm oidc remote execution - 4

--- terraform/accession/permissions.tf
+++ terraform/accession/permissions.tf
@@ -15,3 +15,9 @@ resource "azurerm_role_assignment" "authority_key_vault_reader" {
   role_definition_name = "Reader"
   principal_id         = azuread_service_principal.amiasea_authority_sp.object_id
 }
+
+resource "azurerm_role_assignment" "authority_key_vault_secrets_user" {
+  scope                = azurerm_key_vault.sovereign_kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azuread_service_principal.amiasea_authority_sp.object_id
+}
--- terraform/engineering_foundation/permissions.tf
+++ terraform/engineering_foundation/permissions.tf
@@ -6,6 +6,12 @@ resource "azurerm_role_assignment" "automation_oidc_key_vault_reader" {
   principal_id         = azurerm_user_assigned_identity.uami_amiasea_automation_oidc.principal_id
 }
 
+resource "azurerm_role_assignment" "automation_oidc_key_vault_secrets_user" {
+  scope                = var.sovereign_azure_key_vault_id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.uami_amiasea_automation_oidc.principal_id
+}
+
 # AWS
 
 # GCP
